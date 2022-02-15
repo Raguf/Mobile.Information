bc837b0ca56556ce4268666bdecc434012db3f43
Raguf <ragufym@code.edu.az>
2022-02-15 16:02:36 +0400

    Add project files.

---
+++ MobileData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class MobileData
+    {
+        public int id;
+        public string name;                   // Telefonun markası
+        public int yearOfManufacture;         // Telefonun istehsal ili
+        public string operatingSystem;        // Telefonun əməliyyat sistemi
+        public string operatingSystemVersion; // Telefonun əməliyyat sisteminin versiyası
+        public int numberOfCores;             // Telefonun nüvə sayı
+        public string processorType;          // Telefonun processorun növü
+        public int numberOfSimCards;          // Telefonun SIM kartların sayını
+        public int memoryRAM;                 // Telefonun operativ yaddaşı
+        public int memoryBuiltIn;             // Telefonun daxili yaddaşı
+        public string processorName;          // Telefonun processorun adı
+        public double processorSpeed;         // Telefonun prosessor tezliyi
+        public string network;                // Telefonun şəbəkəsi
+        public string typeOfControl;          // Telefonun idarəetmə növü
+        public double screen;                 // Telefonun ekranın ölçüsü
+        public int camera;                    // Telefonun kamerası
+        public int frontCamera;               // Telefonun ön kamerası
+        public string bodyMaterial;           // Telefonun korpusun materialı
+        public string screenResolution;       // Telefonun ekran görüntüsü
+        public int videoFormat;               // Telefonun video formatı
+        public string batteryType;            // Telefonun batareya növü
+        public int batteryCapacity;           // Telefonun batareyanın həcmi
+        public int warranty;                  // Telefonun zəmanəti
+        public int weight;                    // Çəki
+        public string color;                  // Rəng
+
+        int counter = 0;
+
+        public MobileData()
+        {
+            counter++;
+            id = counter;
+        }
+        public override string ToString()
+        {
+            return $"No: {id}\n" + $"name: {name}\n" + 
+                $"yearOfManufacture: {yearOfManufacture}\n" +
+                $"operatingSystem: {yearOfManufacture}\n" + 
+                $"operatingSystemVersion: {operatingSystemVersion}\n" +
+                $"numberOfCores: {numberOfCores}\n" +
+                $"processorType: {processorType}\n" +
+                $"numberOfSimCards: {numberOfSimCards}\n" +
+                $"memoryRAM: {memoryRAM}\n" +
+                $"memoryBuiltIn: {memoryBuiltIn}\n" +
+                $"processorName: {processorName}\n" +
+                $"processorSpeed: {processorSpeed}\n" +
+                $"network: {network}\n" +
+                $"typeOfControl: {typeOfControl}\n" +
+                $"screen: {screen}\n" +
+                $"camera: {camera}\n" +
+                $"frontCamera: {frontCamera}\n" +
+                $"bodyMaterial: {bodyMaterial}\n" +
+                $"screenResolution: {screenResolution}\n" +
+                $"videoFormat: {videoFormat}\n" +
+                $"batteryType: {batteryType}\n" +
+                $"batteryCapacity: {batteryCapacity}\n" +
+                $"warranty: {warranty}\n" + 
+                $"weight: {weight}\n" +
+                $"color: {color}\n";
+                
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using ClassLibrary.Helper;
+using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Məhsul məlumatlarını saxlamaq üçün tip yaratmalı(class
+               ve linkdeki kimi fieldləri olsun
+               (İnglis dilində yazılmalıdır fieldlər)*/
+
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.Title = "Mobile Information";
+
+            int len; 
+
+        note:
+
+            len = Monitor.ReadInteger("Necə ədəd telefon olsun: ");
+
+            if (len <= 0)
+            {
+                goto note;
+            }
+
+            MobileData [] mobileDatas = new MobileData[len];
+
+            for (int i = 0; i < mobileDatas.Length; i++)
+
+            {
+                MobileData mobileData = new MobileData();
+
+                mobileData.name = Monitor.ReadString($"{i+1}. Telefonun markasını yaz: (Samsung/Nokia/Iphone/Huawei v.s.)");
+                mobileData.yearOfManufacture = Monitor.ReadInt($"{i + 1}. Telefonun istehsal ilini yaz: (il)");
+                mobileData.operatingSystem = Monitor.ReadString($"{i + 1}. Telefonun əməliyyat sistemini yaz: (Android/IOS)");
+                mobileData.operatingSystemVersion = Monitor.ReadString($"{i + 1}. Telefonun əməliyyat sisteminin versiyasını yaz: (1.0-8.0)");
+                mobileData.numberOfCores = Monitor.ReadInt($"{i + 1}. Telefonun nüvə sayını yaz: ");
+                mobileData.processorName = Monitor.ReadString($"{i + 1}. Telefonun processorun adını yaz: ");
+                mobileData.processorType = Monitor.ReadString($"{i + 1}. Telefonun processorun növünü yaz: ");
+                mobileData.processorSpeed = Monitor.ReadInt($"{i + 1}. Telefonun prosessor tezliyini yaz: (GHz)");
+                mobileData.numberOfSimCards = Monitor.ReadInt($"{i + 1}. Telefonun SIM kartların sayını yaz: (1/2)");
+                mobileData.memoryRAM = Monitor.ReadInt($"{i + 1}. Telefonun operativ yaddaşını yaz: (GB)");
+                mobileData.memoryBuiltIn = Monitor.ReadInt($"{i + 1}. Telefonun daxili yaddaşını yaz: (GB)");              
+                mobileData.network = Monitor.ReadString($"{i + 1}. Telefonun şəbəkəsini yaz: (G)");
+                mobileData.typeOfControl = Monitor.ReadString($"{i + 1}. Telefonun idarəetmə növünü yaz: (knopka/sensor)");
+                mobileData.screen = Monitor.ReadDouble($"{i + 1}. Telefonun ekranın ölçüsünü yaz: (')");
+                mobileData.camera = Monitor.ReadInt($"{i + 1}. Telefonun kamerasını yaz: (mpx)");
+                mobileData.frontCamera = Monitor.ReadInt($"{i + 1}. Telefonun ön kamerasını yaz: (mpx)");
+                mobileData.bodyMaterial = Monitor.ReadString($"{i + 1}. Telefonun korpusun materialını yaz: (şüşə/plastik/metal)");
+                mobileData.screenResolution = Monitor.ReadString($"{i + 1}. Telefonun ekran görüntüsünü yaz: (HD)");            
+                mobileData.videoFormat = Monitor.ReadInt($"{i + 1}. Telefonun video formatı yaz: (1K-4K)");
+                mobileData.batteryType = Monitor.ReadString($"{i + 1}. Telefonun batareya növünü yaz: ");
+                mobileData.batteryCapacity = Monitor.ReadInt($"{i + 1}. Telefonun batareyanın həcmini yaz: (mAh)");
+                mobileData.warranty = Monitor.ReadInt($"{i + 1}. Telefonun zəmanətini yaz: (il)");
+                mobileData.weight = Monitor.ReadInt($"{i + 1}. Telefonun çəkisini yaz: (qramm");
+                mobileData.color = Monitor.ReadString($"{i + 1}. Telefonun rəngini yaz: ");
+
+                mobileDatas[i] = mobileData;
+                Console.WriteLine("-----------------------------------------------------");
+
+            }
+
+            Console.WriteLine("########################## List of Mobilephones ###################################");
+
+            foreach (var item in mobileDatas)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("**************************************************");
+            }
+            Console.ReadKey();
+        }
+    }
+}
